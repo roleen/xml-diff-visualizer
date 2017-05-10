7f47481c0d60249a17cb54b4cf2a673dbdbedfc6
Nishant Desai <nishantdesai@wifihost-96-172.wireless.utsc.utoronto.ca>
2017-05-10 18:14:30 -0400

    Fixed generate another diff text overlap

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,6 +51,8 @@ function generateAnotherDiff() {
 function backToEditor() {
     $('#compare').hide();
     $('#inputs').show();
+    baseEditor.refresh();
+    changedEditor.refresh();
 }
 
 function syntaxHighlight() {
